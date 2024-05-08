3e882dd68b2365a0e4def72baa819f900c2b814b
Efe Kurban <efekurbann@gmail.com>
2024-05-08 22:07:56 +0300

    fix typo and add comments

--- middlewares/ratelimit.middleware.js
+++ middlewares/ratelimit.middleware.js
@@ -1,17 +1,19 @@
 const rateLimit = require("express-rate-limit");
 const {Server} = require('../models/server.model');
 
+// non-premium servers can handle ~5 verification per 30 minutes
+// premium servers can handle over 20 verification per 30 minutes
 const serverRateLimiters = {
     false: rateLimit({
         windowMs: 60 * 30 * 1000,
         max: 200,
-        message: {message: "You have exceeded your 200 requests per 30 minute limit."},
+        message: {message: "You have exceeded your 200 requests per 30 minutes limit."},
         headers: true,
     }),
     true: rateLimit({
         windowMs: 60 * 30 * 1000,
         max: 800,
-        message: {message: "You have exceeded your 800 requests per 30 minute limit."},
+        message: {message: "You have exceeded your 800 requests per 30 minutes limit."},
         headers: true,
     }),
 };
