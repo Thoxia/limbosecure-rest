718cfd1f033dd9a30544e6dd1320aa1086a0854f
Efe Kurban <efekurbann@gmail.com>
2024-05-08 04:36:47 +0300

    add: length check for serverId

--- routes/code.js
+++ routes/code.js
@@ -11,6 +11,17 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const body = req.body;
+        body.serverId = req.serverId;
+        const code = await Code.create(body);
+        res.status(201).json(code);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
 router.get('/status/:id', async (req, res) => {
     try {
         const id = req.params.id;
@@ -32,17 +43,6 @@ router.put('/verify/:id', async (req, res) => {
         res.status(200).json({updated: updated.modifiedCount >= 1});
     } catch (err) {
         res.status(500).json({status: false, message: err.message});
-    }
-})
-
-router.post('/', async (req, res) => {
-    try {
-        const body = req.body;
-        body.serverId = req.serverId;
-        const code = await Code.create(body);
-        res.status(201).json(code);
-    } catch (err) {
-        res.status(500).json({message: err.message});
     }
 })
 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(logger)
 app.use(serverIdVerifier)
 
 const codeRouter = require('./routes/code');
-app.use('/v1/code', codeRouter);
+app.use('/api/v1/code', codeRouter);
 
 function logger(req, res, next) {
     console.log(`[${new Date().toLocaleString()}] Got a request to "${req.originalUrl}" from "${req.connection.remoteAddress.split(`:`).pop()}". ServerID: "${req.get("X-API-Key") || "Not Found!"}"`)
@@ -18,7 +18,7 @@ function logger(req, res, next) {
 
 function serverIdVerifier(req, res, next) {
     const serverId = req.get("X-API-Key");
-    if (serverId == null) {
+    if (serverId == null || serverId.length <= 10) {
         res.status(401).json({status: false, message: "You must provide a server id!"})
         return
     }
